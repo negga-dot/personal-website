7e683ba8f738ca31ce1813dd5de7dca0ba392d54
alpha9av <alpha9coder@gmail.com>
2025-07-18 06:52:15 +0530

    error fixed

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [react()],
+    }
+  } else {
+    // Build configuration
+    return {
+      plugins: [react()],
+      build: {
+        rollupOptions: {
+          treeshake: false
+        }
+      }
+    }
+  }
 })
